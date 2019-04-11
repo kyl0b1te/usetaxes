7b214fc051bd4e0a01b53f77dd8aae51d19999bd
zhikiri <zhikiri.dev@gmail.com>
2019-04-11 11:35:57 +0300

    added total credit and tax information

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 
 import { CreditModel } from '../shared/credit.model';
 import * as fromApp from '../store/app.reducer';
+import { AmountModel } from '../shared/amount.model';
 
 @Component({
   selector: 'app-checkout',
@@ -13,6 +14,9 @@ import * as fromApp from '../store/app.reducer';
 export class CheckoutComponent implements OnInit, OnDestroy {
   creditsSubscription: Subscription;
   credits = {};
+
+  totalCredit = new AmountModel(0, 'UAH');
+  totalTax = new AmountModel(0, 'UAH');
 
   constructor(
     private store: Store<fromApp.AppState>
@@ -26,13 +30,17 @@ export class CheckoutComponent implements OnInit, OnDestroy {
   }
 
   groupByQuarter(credits: CreditModel[]) {
+    this.totalCredit.amount = 0;
+    this.totalTax.amount = 0;
     for (const credit of credits) {
       const quarter = credit.quarter;
       if (!this.credits[quarter]) {
         this.credits[quarter] = [];
       }
       this.credits[quarter].push(credit);
+      this.totalCredit.amount += credit.national.amount;
     }
+    this.totalTax.amount = this.totalCredit.amount * 0.05;
   }
 
   ngOnDestroy() {
--- src/app/shared/amount.pipe.ts
+++ src/app/shared/amount.pipe.ts
@@ -2,7 +2,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { AmountModel } from './amount.model';
 import { CurrencyPipe } from '@angular/common';
 
-@Pipe({ name: 'amount' })
+@Pipe({
+  name: 'amount',
+  pure: false
+})
 export class AmountPipe extends CurrencyPipe implements PipeTransform {
   transform(value: AmountModel): string {
     if (!value) {
